cc6e6c5b1c610cb61c59e8aa1fe5388e9baa440a
Peter Zylka <peterzylka@gmail.com>
2013-12-14 02:38:07 -0600

    It compiles now
    Sam, try it now. This is now ready to add as an import, though still
    needs some more functionality. (woot)

--- src/main/Metadata.py
+++ src/main/Metadata.py
@@ -6,43 +6,42 @@ from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
 import mutagen
 
-inputPath = "C:\Users\Peter Zylka\Desktop\Test\Input"
-outputPath = "C:\Users\Peter Zylka\Desktop\Test\Output"
-
-#print(listdir(inputPath))
-
-
-
-
-
-songName = "01 Artist - Track 1.m4a"
-songName2 = "01 Track 1.mp3"
-songName3 = "Layla.mp3"
-
-#print(os.stat(inputPath + "\\" + songName))
-#print(os.st_flags(inputPath + "\\" + songName))
-
-audio = EasyID3(inputPath + "\\" + songName3)
-print(audio['date'])
-print(audio)
-
-try:
-	print(audio['stuff'])
-except:
-	print('')
 
 class SongHandler:
 	def __init__(self, path):
-		super(ClassName, self).__init__()
 		self.audio = EasyID3(path)
 
-	def getVal(fieldIn):
+	def getVal(self, fieldIn):
 		try:
 			return audio[fieldIn]
 		except:
 			return ''
 
-	def setVal(fieldIn, valIn):
+	def setVal(self, fieldIn, valIn):
 		audio[fieldIn] = valIn
-		audio.save
-		
+		audio.save()
+
+if __name__ == "__main__":
+	inputPath = "../../../Test/Input/"
+	
+	#You should probably make a Test/Input folder directly above the 
+	#spyglass folder
+
+	songName = "01 Artist - Track 1.m4a"
+	songName2 = "01 Track 1.mp3"
+	songName3 = "Layla.mp3"
+
+	#print(os.stat(inputPath + "\\" + songName))
+	#print(os.st_flags(inputPath + "\\" + songName))
+
+	audio = EasyID3(inputPath + "\\" + songName3)
+	print(audio['date'])
+	print(audio)
+
+	try:
+		print(audio['stuff'])
+	except:
+		print('')
+
+	newSongClass = SongHandler(inputPath + "\\" + songName3)
+	print(newSongClass.getVal('date'))
